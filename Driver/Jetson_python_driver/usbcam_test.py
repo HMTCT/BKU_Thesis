8bad10f076326cf5217ba7517ba39e2c28dd8eca
potato-stack <hatrungquyen89@gmail.com>
2024-03-17 17:21:37 +0700

    coors update and offset mod

--- Driver/Jetson_python_driver/usbcam_test.py
+++ Driver/Jetson_python_driver/usbcam_test.py
@@ -15,27 +15,24 @@ import os
 import sys
 import serial
 
-# The matrix is obtained at Z = 241
-K_values = [[531.9701860236803,    0.0,               291.67966784917843],
- [  0.0,               531.6495748957748,  228.32995564225655],
+# The matrix is obtained at Z = 141
+K_values = [[521.2983893968932 ,    0.0,               310.1165090086347],
+ [  0.0,                520.4297206491902 , 220.21613563458055],
  [  0.0 ,                0.0    ,             1.0              ]]
 # Values for R
-R_values = [
-[[0.24112166743599],
- [0.39540689825273],
- [1.54230974041242]]
-]
+R_values = [[-0.01470173416359],
+ [ 0.05877006788942],
+ [ 1.48890898522158]]
 
 # Values for T
-T_values = [
-[ 2.60211972765208],
- [-3.98323538156345],
- [19.78538934703026],
-]
+T_values  = [[ -1.53772720717909],
+ [-24.1415269908699 ],
+ [227.88669936031886]]
 
 K_array = np.array(K_values)
 R_array = np.array(R_values)
 T_array = np.array(T_values)
+
 def camera_to_world(cam_mtx, r, t, img_points):
     inv_k = np.asmatrix(cam_mtx).I
     r_mat = np.zeros((3, 3), dtype=np.float64)
@@ -75,24 +72,28 @@ def world_to_arm(arm_coors, img_coors):
     des_coors = np.array(trans_mtx) @ np.array(img_coors)
     return des_coors
 
-# serialPort = 'COM3'
-# serialBaudrate = 115200
-# ack = False
-# debounce = 0
-# stopFlag = False
-# Initialize the joystick
+serialPort = 'COM4'
+serialBaudrate = 115200
+ack = False
+debounce = 0
+stopFlag = False
 
-# serialObject = serial.Serial(
-#     port=serialPort, 
-#     baudrate=serialBaudrate,
-#     bytesize=serial.EIGHTBITS,
-#     parity=serial.PARITY_NONE,
-#     stopbits=serial.STOPBITS_ONE, 
-#     timeout=0.01
-# ) 
-# time.sleep(10)
 
-cap = cv2.VideoCapture(0)
+serialObject = serial.Serial(
+    port=serialPort, 
+    baudrate=serialBaudrate,
+    bytesize=serial.EIGHTBITS,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE, 
+    timeout=0.01
+) 
+time.sleep(10)
+serialObject.write(bytes(str("!Initon#"), encoding='utf-8'))
+time.sleep(10)
+serialObject.write(bytes(str("!goauto#"), encoding='utf-8'))
+
+
+cap = cv2.VideoCapture(1)
 
 if torch.cuda.is_available():
     print("Confirm CUDA recognized")
@@ -110,21 +111,22 @@ except Exception as e:
 
 
 # Image center: 240 320
+# Home coordinates: 164.5, 0.0, 141.0, 90.0, 180.0, -90.0
 objects_list = []
 IMAGE_PROC = 1
 GRAP = 2
 STORE = 3
-ILDE = 0
+IDLE = 0
 state = IMAGE_PROC
 while cap.isOpened():
-    # data = serialObject.readline().decode(encoding='utf-8')
-    # if data == "ACK\r\n": 
-    #     print("acknowledage")
-    #     ack = True
-    # elif data == "KCA\r\n": 
-    #     print("stopped")
-    #     ack = True
-    # if ack == False: continue
+    data = serialObject.readline().decode(encoding='utf-8')
+    if data == "ACK\r\n": 
+        print("acknowledage")
+        ack = True
+    elif data == "KCA\r\n": 
+        print("stopped")
+        ack = True
+    if ack == False: continue
 
     if state == IMAGE_PROC:
         ret, frame = cap.read()
@@ -152,12 +154,14 @@ while cap.isOpened():
                     for box in objects:
                         print(box)
                         *coordinates, _ = camera_to_world(K_array, R_array, T_array, np.array([box[0:2]]).reshape((1, 1, 2)))[0][0]
-                        arm_coordinates = world_to_arm([165, 0, 242], [[coordinates[0]], [coordinates[1]], [0], [1]])
+                        arm_coordinates = world_to_arm([165, 0, 141], [[coordinates[0]], [coordinates[1]], [0], [1]])
                         temp_list.append(arm_coordinates)
                         print("dist", arm_coordinates)
                     if len(objects_list) == 0: 
                         objects_list = temp_list
                     else: 
+                        state = GRAP
+                        print(temp_list)
                         print("Have to compared here")
                 except Exception as e:
                     continue
@@ -168,6 +172,18 @@ while cap.isOpened():
 
             if cv2.waitKey(1000) & 0xFF == ord('q'):
                 break
+    if state == GRAP:
+        object = objects_list.pop()
+        angle = np.arctan(object[1][0]/object[0][0])
+        y = object[1][0]+ np.sin(angle)*30
+        x = object[0][0]+ np.cos(angle)*30
+        #string = "!" + str(object[0][0]) + ":" +  str(object[1][0]) + ":47:90:180:-90"
+        string = "!" + str(x) + ":" +  str(y) + ":47:90:180:-90"
+        string += "#"
+        print('send: ', string)
+        serialObject.write(bytes(str(string), encoding='utf-8'))
+        ack = False
+        state = IDLE
             
 cap.release()
 cv2.destroyAllWindows()
